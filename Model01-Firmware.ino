66acf9b61ba1416de2c6d6d67ec82cfc6de772ac
Gabriel Adomnicai <gabesoft@gmail.com>
2019-01-16 22:29:03 -0800

    Revert "Fn + Ctrl = Command"
    This reverts commit f799ea9fddcb52a215f9a482cd52d6920f330511.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -268,14 +268,14 @@ KEYMAPS(
    Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
    Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
    Key_End,  Key_PrintScreen,  Key_Insert,  Key_Escape, Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
-   Key_RightGui, Key_Delete, ___, ___,
+   ___, Key_Delete, ___, ___,
    ___,
 
    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
    Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
                                Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  Key_LeftParen,    Key_RightParen,
    Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
-   ___, ___, Key_Enter, Key_LeftGui,
+   ___, ___, Key_Enter, ___,
    ___)
 ) // KEYMAPS(
 
